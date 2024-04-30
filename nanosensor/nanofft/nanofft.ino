a72d10439ed0d49d420d2774de3ebca9e869afa1
pedro25m <pedropetar@gmail.com>
2024-04-30 12:45:09 +0100

    df

---
+++ nanosensor/nanofft/nanofft.ino
@@ -0,0 +1,89 @@
+#include <WiFiNINA.h>
+#include <PDM.h>
+
+#define BUTTON_PIN 10
+
+//Inicialização de variáveis
+
+bool startLoop = false;
+
+bool LED_SWITCH = false; //Booleana para controlar o led 
+
+static const char channels = 1; // Numero de canais de saída
+
+static const int frequency = 25000; // frequência de amostragem (PCM output) 16kHz 
+// (verificar se podemos usar uma frequência mais alta para termos dados com mais qualidade mas um programa mais pesado)
+
+short sampleBuffer[512]; // Buffer to armazenar as amostras, cada uma de 16 bits
+
+int samplesRead; // Numero de amostras lidas
+
+int readcounter = 0;
+
+////Funções
+
+//Função para processar os dados do microfone pdm
+void onPDMdata() {
+  // Número de bytes disponíveis
+  int bytesAvailable = PDM.available();
+
+  // Ler os dados
+  PDM.read(sampleBuffer, bytesAvailable);
+
+  // 16 bits ou seja 2 bytes por amostra
+  samplesRead = bytesAvailable / 2;
+}
+
+
+
+//Setup
+void setup() {
+  Serial.begin(9600);
+  pinMode(LEDB, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  while (!Serial);
+  // Chamar a função que lê os dados à medida que vamos recebendo novos dados
+  PDM.onReceive(onPDMdata);
+
+  // >Ganho (ver se é necessário)
+  // PDM.setGain(30);
+
+  // Inicializar PDM (Pulse Density Modulation) com o número de canais e frequência de amostragem
+  if (!PDM.begin(channels, frequency)) {
+    Serial.println("Failed to start PDM!");
+    while (1);
+  }
+}
+
+//Loop
+void loop() {
+
+  if (digitalRead(BUTTON_PIN) == HIGH ) {
+    startLoop = true;
+    delay(500); //avoid repeating the loop multiple times because of how long the button is pressed for
+    readcounter = 0;
+    Serial.println("start");
+  }
+
+
+  if (samplesRead && startLoop) {
+
+    // Fazer print das amostras no serial plotter/monitor
+    for (int i = 0; i < samplesRead && startLoop == true; i++) {
+      if (channels != 1) {
+        Serial.print("Erro");
+      }
+
+      Serial.println(sampleBuffer[i]);
+      if (readcounter >= 10000) {  //130000 at 44kHz, 40000 at 16kHz
+        startLoop = false;
+        Serial.println("finish");
+      }
+      readcounter++;
+    }
+
+    // Recomeçar a contagem
+    samplesRead = 0;
+  }
+}
+   
