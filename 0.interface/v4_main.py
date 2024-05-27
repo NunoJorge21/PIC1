2b2bd196ce94d007f145e3c2a84ce1c00b1f306f
zurc <afonsobpcruz@gmail.com>
2024-05-27 20:33:09 +0100

    update

--- 0.interface/v4_main.py
+++ 0.interface/v4_main.py
@@ -1,131 +1,83 @@
 import plotly.graph_objects as go
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output, callback_context
-import numpy
 
+HALF_SAMP_FREQ = 20000  # 20 kHz
+SPECTRAL_RESOLUTION = 10  # Hz
 
-HALF_SAMP_FREQ = 20000 # 20 kHz
-SPECTRAL_RESOLUTION = 10 # Hz
-
-###########################
-# !!!!!!!!!!!!!!!!!!!!!!!!!
-# Temos de ter a certeza de que isto é sempre verdade!
-freq_axis = numpy.linspace(0, HALF_SAMP_FREQ, num=int(HALF_SAMP_FREQ/SPECTRAL_RESOLUTION)+1, endpoint=True).tolist()
-y_data = [-170]*2001
-y_data[100] = 20
-
+# Função para simular dados do Arduino
 def getDataFromArduino():
+    latitude = 38.736667
+    longitude = -9.13722
+    freq_data = [-170] * 2001
+    freq_data[100] = 20
+    return pd.DataFrame({'Latitude': [latitude], 'Longitude': [longitude], 'Intensity_dB': [max(freq_data)]})
 
-    data = {'Latitude': [38.736667, 38.736944, 38.736944, 38.736667], 
-            'Longitude': [-9.137222, -9.137222, -9.138333, -9.138333],
-            'Intensity_dB': [120, 120, 30, 30]}
-    return pd.DataFrame(data)
-
+# Inicializar dados
 df = getDataFromArduino()
 
-# Create a scattermapbox trace
-trace = go.Scattermapbox(
-    lat=df['Latitude'],
-    lon=df['Longitude'],
-    mode='markers',
-    marker=dict(
-        size=20,
-        color=df['Intensity_dB'],
-        opacity=0.8,
-        colorbar=dict(
-            title='Intensity (dB)',
-            titleside='right',
-            ticks='outside',
-        ),
-        colorscale='RdYlGn',
-        reversescale=True  # Reverse the colorscale to start with green and end with red
-    ),
-    text=[f'Button {i+1}' for i in range(len(df))],
-    customdata=[i for i in range(len(df))]  # Custom data to identify buttons
-)
-
-# Create a layout for the figure
-layout = go.Layout(
-    title='Sound Monitoring Scatter Map',
-    mapbox=dict(
-        center=dict(lat=df['Latitude'].mean(), lon=df['Longitude'].mean()),
-        zoom=18,
-        #style='open-street-map'
-        style='carto-positron'
-    ),
-    margin=dict(r=0, t=50, l=0, b=10)
-)
-
-# Create the figure
-fig = go.Figure(data=[trace], layout=layout)
-
-# Initialize the Dash app with suppress_callback_exceptions=True
-app = Dash(__name__, suppress_callback_exceptions=True)
-
-# Define the app layout
+# Layout principal
+app = Dash(__name__, suppress_callback_exceptions=True, title='Smart Sound Monitoring')
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
+    # Barra de navegação
+    html.Div([
+        html.Button("Sound Monitoring Scatter Map", id="scatter-button", style={'margin': '0 10px'}),
+        html.Button("Layout Edit", id="layout-button", style={'margin': '0 10px'})
+    ]),
+    # Conteúdo das seções
     html.Div(id='page-content')
 ])
 
-# Callback to update the URL based on button click
+# Página do mapa de dispersão de monitoramento de som
+scatter_map_page = html.Div([
+    dcc.Graph(id='main-graph', figure={}, style={'height': '80vh'})  # Mapa de dispersão
+])
+
+# Página de edição de layout
+layout_edit_page = html.Div([
+    html.H3('Layout Edit'),
+    # Conteúdo da edição de layout aqui
+])
+
+# Callback para atualizar a URL com base no clique nos botões de navegação
 @app.callback(
     Output('url', 'pathname'),
-    [Input(f'button-{i+1}', 'n_clicks') for i in range(len(df))]
+    [Input('scatter-button', 'n_clicks'), Input('layout-button', 'n_clicks')]
 )
-def update_url(*args):
+def update_url(scatter_clicks, layout_clicks):
     ctx = callback_context
     if not ctx.triggered:
         return '/'
-    else:
-        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-        return f'/plot/{button_id}'
-
-# Callback to display content based on URL
-@app.callback(
-    Output('page-content', 'children'),
-    Input('url', 'pathname')
-)
-def display_page(pathname):
-    if pathname == '/':
-        return html.Div([
-            dcc.Graph(id='main-graph', figure=fig),
-            html.Div(id='button-container', children=[
-                html.Button(f'Button {i+1}', id=f'button-{i+1}', n_clicks=0) for i in range(len(df))
-            ])
-        ])
-    elif pathname and pathname.startswith('/plot/'):
-        button_id = pathname.split('/')[-1]
-        button_index = button_id.split('-')[-1]
-        return html.Div([
-            html.H3(f'Displaying plot for {button_id}'),
-            dcc.Graph(
-                figure=go.Figure(
-                    data=[
-                        go.Scatter(
-                            x=freq_axis,
-                            y=y_data
-                        )
-                    ],
-                    layout=go.Layout(
-                        title=f'Plot for {button_id}',
-                        xaxis=dict(title='Frequency [Hz]'),
-                        yaxis=dict(title='Intensity [dB]')
-                    )
-                )
-            ),
-            html.Button("Back to Main Map", id="back-button", n_clicks=0)
-        ])
-    return "404 Page Not Found"
-
-# Callback to handle the back button
-@app.callback(
-    Output('url', 'pathname', allow_duplicate=True),
-    Input('back-button', 'n_clicks'),
-    prevent_initial_call=True
-)
-def go_back(n_clicks):
+    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    if button_id == 'scatter-button':
+        return '/scatter-map'
+    elif button_id == 'layout-button':
+        return '/layout-edit'
     return '/'
 
+# Callback para exibir o conteúdo com base na URL
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('url', 'pathname')]
+)
+def display_content(pathname):
+    if pathname == '/scatter-map':
+        return scatter_map_page
+    elif pathname == '/layout-edit':
+        return layout_edit_page
+    else:
+        return scatter_map_page  # Página padrão
+
+# Callback para destacar o botão de navegação da seção atual
+@app.callback(
+    Output('scatter-button', 'style'),
+    [Input('url', 'pathname')]
+)
+def highlight_button(pathname):
+    if pathname == '/scatter-map':
+        return {'margin': '0 10px', 'background-color': 'lightblue'}
+    return {'margin': '0 10px'}
+
 if __name__ == '__main__':
-    app.run_server(debug=True, use_reloader=False)
+    app.run_server(debug=True, use_reloader=False, port=8055)
